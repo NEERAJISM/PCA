24c7be039497105ac7f3bef9768c7ef198376695
NEERAJISM <8patidarneeraj@gmail.com>
2016-08-07 12:34:40 +0530

    first commit reducing the dimension of input data from 3d to 2d

---
+++ pcaMain.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+# generating random data of 3 dimension
+np.random.seed(42949672)
+
+mu_vec1 = np.array([0,0,0])
+cov_mat1 = np.array([[1,0,0],[0,1,0],[0,0,1]])
+class1_sample = np.random.multivariate_normal(mu_vec1, cov_mat1, 20).T
+assert class1_sample.shape == (3,20), "The matrix has not the dimensions 3x20"
+
+mu_vec2 = np.array([1,1,1])
+cov_mat2 = np.array([[1,0,0],[0,1,0],[0,0,1]])
+class2_sample = np.random.multivariate_normal(mu_vec2, cov_mat2, 20).T
+assert class1_sample.shape == (3,20), "The matrix has not the dimensions 3x20"
+
+all_samples = np.concatenate((class1_sample, class2_sample), axis=1)
+assert all_samples.shape == (3,40), "The matrix has not the dimensions 3x40"
+
+#print sample dataset of 3 dimension
+print("sample data set\n")
+print(all_samples)
+
+#Computing the 3-dimensional mean vector
+mean_x = np.mean(all_samples[0,:])
+mean_y = np.mean(all_samples[1,:])
+mean_z = np.mean(all_samples[2,:])
+
+mean_vector = np.array([[mean_x],[mean_y],[mean_z]])
+print('Mean Vector:\n', mean_vector)
+
+#computing the scatter matrix
+scatter_matrix = np.zeros((3,3))
+for i in range(all_samples.shape[1]):
+    scatter_matrix += (all_samples[:,i].reshape(3,1) - mean_vector).dot((all_samples[:,i].reshape(3,1) - mean_vector).T)
+print('Scatter Matrix:\n', scatter_matrix)
+
+#computing the covariance matrix
+cov_mat = np.cov([all_samples[0,:],all_samples[1,:],all_samples[2,:]])
+print('Covariance Matrix:\n', cov_mat)
+
+# eigen vectors and eigenvalues for the from the scatter matrix
+eig_val_sc, eig_vec_sc = np.linalg.eig(scatter_matrix)
+
+# eigen vectors and eigenvalues for the from the covariance matrix
+eig_val_cov, eig_vec_cov = np.linalg.eig(cov_mat)
+
+#print eigen vectors
+for i in range(len(eig_val_sc)):
+    eigvec_sc = eig_vec_sc[:,i].reshape(1,3).T
+    eigvec_cov = eig_vec_cov[:,i].reshape(1,3).T
+    assert eigvec_sc.all() == eigvec_cov.all(), 'Eigenvectors are not identical'
+
+    print('Eigenvector {}: \n{}'.format(i+1, eigvec_sc))
+    print('Eigenvalue {} from scatter matrix: {}'.format(i+1, eig_val_sc[i]))
+    print('Eigenvalue {} from covariance matrix: {}'.format(i+1, eig_val_cov[i]))
+    print('Scaling factor: ', eig_val_sc[i]/eig_val_cov[i])
+    print(40 * '-')
+
+
+#checking the calculation of eigen vectors and eigen values
+for i in range(len(eig_val_sc)):
+    eigv = eig_vec_sc[:,i].reshape(1,3).T
+    np.testing.assert_array_almost_equal(scatter_matrix.dot(eigv), eig_val_sc[i] * eigv, decimal=6, err_msg='', verbose=True)
+
+for ev in eig_vec_sc:
+    np.testing.assert_array_almost_equal(1.0, np.linalg.norm(ev))
+    # instead of 'assert' because of rounding errors
+
+# Make a list of (eigenvalue, eigenvector) tuples
+eig_pairs = [(np.abs(eig_val_sc[i]), eig_vec_sc[:,i]) for i in range(len(eig_val_sc))]
+
+# Sort the (eigenvalue, eigenvector) tuples from high to low
+eig_pairs.sort(key=lambda x: x[0], reverse=True)
+
+#confirm that the list is correctly sorted by decreasing eigenvalues
+for i in eig_pairs:
+    print(i[0])
+
+
+# generating the output of the reduced dimension d = 2
+matrix_w = np.hstack((eig_pairs[0][1].reshape(3,1), eig_pairs[1][1].reshape(3,1)))
+print('Matrix W:\n', matrix_w)
+
+transformed = matrix_w.T.dot(all_samples)
+assert transformed.shape == (2,40), "The matrix is not 2x40 dimensional."
